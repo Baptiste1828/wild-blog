39356af54ae3898cf91575ab9343482483628ea1
Baptiste Delefosse <baptiste.delefosse@hotmail.fr>
2024-10-03 23:51:01 +0200

    Angular 08 : Communication entre les composants : @Input

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,25 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Article } from '../Model/Article';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
-  article: Article = {
-    id:4,
-    title: 'Titre de l\'article',
-    author: 'John Doe',
-    content: 'Voici le contenu de l\'article.',
-    image: 'https://via.placeholder.com/350x150',
-    isPublished: true,
-    comment: '',
-    likes: 1000
-  };
+
+  @Input() article!: Article;
 
   togglePublication(): void {
     this.article.isPublished = !this.article.isPublished;
