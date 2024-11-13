e6f480180d6ecaa518fc415073b5b165867ef325
Baptiste Delefosse <baptiste.delefosse@hotmail.fr>
2024-11-13 18:11:00 +0100

    Angular 10 : Les requêtes HTTP

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,21 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Article } from '../../models/article.model';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.scss'
 })
 export class ArticlePageComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
+  http: HttpClient = inject(HttpClient);
+
   articleId!: number;
+  article!: Article;
+  articleSubscription!: Subscription;
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
     });
+    this.articleSubscription = this.getArticleById(this.articleId);
+  }
+
+  getArticleById(id: number) {
+    return this.http.get<Article>(`http://localhost:3000/articles/${id}`).subscribe(data => {
+      this.article = data;
+    });
+  }
+
+  ngDestroy() {
+    this.articleSubscription.unsubscribe();
   }
 }
